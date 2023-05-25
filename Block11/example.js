e370133167d10baba532beb1668d81b31832cc03
Theads1 <tiaradheads@gmail.com>
2023-05-25 18:47:39 -0400

    more practice

--- Block11/example.js
+++ Block11/example.js
@@ -13,6 +13,8 @@ let array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 // };
 // console.log(result);
 
-for (let i=array.length-1; i>=0; i--){
-    console.log(array[i]);
-}
+// for (let i=array.length-1; i>=0; i--){
+//     console.log(array[i]);
+// }
+
+console.log(Math.max(...array))
