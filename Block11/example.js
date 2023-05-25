aa4e4dab03f4ef2506f46c7f29e5751e6bd123f1
Theads1 <tiaradheads@gmail.com>
2023-05-25 18:39:57 -0400

    context practice

--- Block11/example.js
+++ Block11/example.js
@@ -1,5 +1,18 @@
 // create variable Num
 // equal 2*3
 // show value through an alert
-let num=2*3
-alert(num)
+// let num=2*3
+// alert(num)
+let array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let result = []
+// for(let i=0;i<array.length; i++){
+//     if (array[i] % 2===1){
+//         result.push(array[i]);
+//     }
+//     continue;
+// };
+// console.log(result);
+
+for (let i=array.length-1; i>=0; i--){
+    console.log(array[i]);
+}
