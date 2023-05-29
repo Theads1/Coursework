329d58f5c58d9926a49a6a54663df667f3617156
Theads1 <tiaradheads@gmail.com>
2023-05-29 15:45:41 -0400

    intermed. file creation

--- More Practice/Beginner BroCode/counter.js
+++ More Practice/Beginner BroCode/counter.js
@@ -11,3 +11,22 @@ document.getElementById("increaseBtn").onclick=function(){
     count+=1;
 document.getElementById("countLabel").innerHTML=count;
 }
+
+// if else statement with tenary
+
+let adult= checkAge(21)
+function checkAge(age){
+    if (age>= 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// or
+
+function checkAge(age){
+    return age >= 18 ? true : false
+    //condition ? return if true : vs if false
+}
